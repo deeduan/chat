02e070d8dd08e611657baba3021d1c50f7a1ebc8
duan you <duanyou@huishoubao.com.cn>
2019-10-11 19:54:07 +0800

    add socket

--- bin/test_websocket.php
+++ bin/test_websocket.php
@@ -13,50 +13,54 @@ use Workerman\Worker;
 use Workerman\Lib\Timer;
 use Workerman\Connection\AsyncTcpConnection;
 
-$worker = new Worker();
+
+//for ($i = 0; $i < 2000; $i++) {
+
+    $worker = new Worker();
 // 单进程
-$worker->count = 1;
+    $worker->count = 1;
 
-$worker->onWorkerStart = 'connect';
-function connect(){
-    static $count = 0;
+    $worker->onWorkerStart = 'connect';
+    function connect() {
+//        static $count = 0;
+//
+//        // 2000个链接
+//        if ($count++ >= 2000) return;
+        // 建立异步链接
+        $con = new AsyncTcpConnection('ws://127.0.0.1:8282');
 
-    // 2000个链接
-    if ($count++ >= 2000) return;
-    // 建立异步链接
-    $con = new AsyncTcpConnection('ws://127.0.0.1:8282');
+        // 顺序建立链接
+        $con->onConnect = function($con) {
+            connect();
+        };
 
-    // 顺序建立链接
-    $con->onConnect = function($con) {
-        connect();
-    };
+        $con->onMessage = function($con, $msg) {
+            echo "recv $msg\n";
+        };
 
-    $con->onMessage = function($con, $msg) {
-        echo "recv $msg\n";
-    };
+        $con->onClose = function($con) {
+            echo "con close\n";
+        };
 
-    $con->onClose = function($con) {
-        echo "con close\n";
-    };
+        // 定时器 每10s发送一个消息
+        Timer::add(10, function()use($con){
+            $con->send("ping test");
+        });
 
-    // 定时器 每10s发送一个消息
-    Timer::add(10, function()use($con){
-        $con->send("ping test");
+        $con->connect();
+
+        echo " connections complete\r\n";
+    }
+
+    // 压测10分钟  10分钟后关掉客户端
+    Timer::add(10 * 60, function(){
+        file_put_contents('/tmp/workerman_stop', time());
+
+        $master_pid = \is_file(Worker::$pidFile) ? \file_get_contents(Worker::$pidFile) : 0;
+
+        // 给客户端发送一个平滑停止信号
+        \posix_kill($master_pid, SIGTERM);
     });
-
-    $con->connect();
-
-    echo $count, " connections complete\r\n";
-}
-
-// 压测10分钟  10分钟后关掉客户端
-Timer::add(10 * 60, function(){
-    file_put_contents('/tmp/workerman_stop', time());
-
-    $master_pid = \is_file(Worker::$pidFile) ? \file_get_contents(Worker::$pidFile) : 0;
-
-    // 给客户端发送一个平滑停止信号
-    \posix_kill($master_pid, SIGTERM);
-});
+//}
 
 Worker::runAll();
