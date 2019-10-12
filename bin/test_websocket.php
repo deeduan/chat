97dae35324390263a38e0a64fcbe30a0a78612a5
duan you <duanyou@huishoubao.com.cn>
2019-10-12 19:36:52 +0800

    satus

--- bin/test_websocket.php
+++ bin/test_websocket.php
@@ -14,36 +14,38 @@ use Workerman\Worker;
 use Workerman\Lib\Timer;
 use Workerman\Connection\AsyncTcpConnection;
 
+function connect(){
+    static $count = 0;
+    // 2000个链接
+    if ($count++ >= 230) return;
+    // 建立异步链接
+    $con = new AsyncTcpConnection('ws://127.0.0.1:8282');
+    $con->tipNumber = $count;
+    $con->onConnect = function($con) {
+        // 递归调用connect
+        connect();
+    };
+    $con->onMessage = function($con, $msg) {
+        // echo "recv $msg\n";
+    };
+
+    $con->onClose = function($con) {
+        echo "con close $con->tipNumber \n";
+    };
+
+    // 当前链接每10秒发个心跳包
+    Timer::add(1, function()use($con){
+        $con->send("ping");
+    });
+    $con->connect();
+    echo $count, " connections complete\n";
+}
+
 for ($i = 0; $i < 10; $i++) {
 
     $worker = new Worker();
     $worker->onWorkerStart = 'connect';
-    function connect(){
-        static $count = 0;
-        // 2000个链接
-        if ($count++ >= 230) return;
-        // 建立异步链接
-        $con = new AsyncTcpConnection('ws://127.0.0.1:8282');
-        $con->tipNumber = $count;
-        $con->onConnect = function($con) {
-            // 递归调用connect
-            connect();
-        };
-        $con->onMessage = function($con, $msg) {
-            // echo "recv $msg\n";
-        };
 
-        $con->onClose = function($con) {
-            echo "con close $con->tipNumber \n";
-        };
-
-        // 当前链接每10秒发个心跳包
-        Timer::add(1, function()use($con){
-            $con->send("ping");
-        });
-        $con->connect();
-        echo $count, " connections complete\n";
-    }
 }
 
 Worker::runAll();
